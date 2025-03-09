a5cf9fa5be6b97d92da97d8fe06e37ea77ccea0b
ZlatinK <zlatinkostadinow@gmail.com>
2025-03-09 20:43:32 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        System.out.println("Please enter your name");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userName = scanner.nextLine();
+
+        System.out.println("Please enter your age:");
+
+        int useAge = scanner.nextInt();
+
+        System.out.println("Please enter your hoby:");
+
+        Scanner scanner1 = new Scanner(System.in);
+
+        String userHoby = scanner1.nextLine();
+
+        String newLine = System.getProperty("line.separator");
+
+        System.out.println("Nice to meet you " + userName +
+                ", what a wonderful age " + useAge + "!!!" + newLine +
+                 userHoby + ". That is a great entertainment!");
+    }
+}
